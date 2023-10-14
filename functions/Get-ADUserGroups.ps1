df3a8f9c211d1f47565bf012c784f716a9278275
scottfromspace <scott.serpenz@gmail.com>
2023-10-14 21:10:21 +1300

    Create Get-ADUserGroups.ps1

---
+++ functions/Get-ADUserGroups.ps1
@@ -0,0 +1,16 @@
+﻿#Serpenz Software, https://www.serpenz.co.nz/
+
+$user = Read-Host "Enter user 1's username"
+
+$userGroups = Get-ADPrincipalGroupMembership $user1 | Select-Object -ExpandProperty Name
+
+$userGroups = $userGroups | Sort-Object
+
+clear
+
+Write-Host "$user's AD groups:"
+$userGroups | ForEach-Object {
+    Write-Host $_ -ForegroundColor Yellow
+}
+
+Read-Host "Press ENTER key to EXIT"
