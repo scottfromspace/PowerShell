fc2e6b70304aae6001f1b0d240604583a1fbd5ca
scottfromspace <scott.serpenz@gmail.com>
2023-10-19 21:26:36 +1300

    Update Get-ADUserGroups.ps1 to include an array of user groups to highlight in yellow, allowing easy identification of important groups

--- scripts/Get-ADUserGroups.ps1
+++ scripts/Get-ADUserGroups.ps1
@@ -1,16 +1,23 @@
 ﻿#Serpenz Software, https://www.serpenz.co.nz/
 
-$user = Read-Host "Enter user 1's username"
+$user = Read-Host "Enter user's username"
 
-$userGroups = Get-ADPrincipalGroupMembership $user1 | Select-Object -ExpandProperty Name
+$userGroups = Get-ADPrincipalGroupMembership $user | Select-Object -ExpandProperty Name
 
 $userGroups = $userGroups | Sort-Object
+
+$highlightedGroups = @("Group1", "Group2", "Group3")  # Add your group names here
 
 clear
 
 Write-Host "$user's AD groups:"
 $userGroups | ForEach-Object {
-    Write-Host $_ -ForegroundColor Yellow
+    $groupName = $_
+    if ($highlightedGroups -contains $groupName) {
+        Write-Host $groupName -ForegroundColor Yellow
+    } else {
+        Write-Host $groupName
+    }
 }
 
 Read-Host "Press ENTER key to EXIT"
