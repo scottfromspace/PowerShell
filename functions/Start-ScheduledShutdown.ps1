7ff6148d4dd15e85741945529bf973d0bb28bdfd
scottfromspace <scott.serpenz@gmail.com>
2023-10-21 21:56:32 +1300

    Create Start-ScheduledShutdown.ps1

---
+++ functions/Start-ScheduledShutdown.ps1
@@ -0,0 +1,33 @@
+﻿#Serpenz Software, https://www.serpenz.co.nz/
+
+function Compare-ADUsers {
+    param(
+        [Parameter(Mandatory=$true)]
+        [ValidateSet("Shutdown", "Restart", "Hibernate", "Log off", "Abort")]
+        [string]$Task,
+        [string]$Hours,
+        [string]$Minutes,
+        [string]$Seconds
+    )
+
+    if($Task -eq "Shutdown")
+    {
+        shutdown -s
+    }
+    elseif($Task -eq "Restart")
+    {
+        shutdown -r
+    }
+    elseif($Task -eq "Hibernate")
+    {
+        shutdown -h
+    }
+    elseif($Task -eq "Log off")
+    {
+        shutdown -l
+    }
+    elseif($Task -eq "Abort")
+    {
+        shutdown -a
+    }
+}
