69c365c6eeadd9e733f5b86739792d5c7e8b233f
scottfromspace <scott.serpenz@gmail.com>
2023-10-21 22:35:28 +1300

    Update Start-ScheduledShutdown.ps1

--- functions/Start-ScheduledShutdown.ps1
+++ functions/Start-ScheduledShutdown.ps1
@@ -1,37 +1,51 @@
 ﻿#Serpenz Software, https://www.serpenz.co.nz/
+#Schedules a shutdown without the annoying warning before execution, and allows user to more easily set a time
 
 function Start-ScheduledShutdown {
     param(
         [Parameter(Mandatory=$true)]
         [ValidateSet("Shutdown", "Restart", "Hibernate", "Log off", "Abort")]
         [string]$Task,
-        [string]$Hours,
-        [string]$Minutes,
-        [string]$Seconds,
-        [bool]$DisableWarning
+        [int]$Hours,
+        [int]$Minutes,
+        [int]$Seconds
     )
 
-    $time = 0
-    $time = $time + ((($Hours * 60) * 60) + ($Minutes * 60) + ($Seconds))
+    $Time = 0
+    $Time = $Time + ((($Hours * 60) * 60) + ($Minutes * 60) + ($Seconds))
 
-    if($Task -eq "Shutdown")
+    if($Task -notlike "Abort")
     {
-        shutdown -s -f -t $time
+        $scriptBlock = {
+            Param (
+                [string]$Task,
+                [string]$Time
+            )
+
+            if($Task -like "Shutdown")
+            {
+                shutdown -s -f -t 0
+            }
+            elseif($Task -like "Restart")
+            {
+                shutdown -r -f -t 0
+            }
+            elseif($Task -like "Log off")
+            {
+                shutdown -l -f -t 0
+            }
+            elseif($Task -like "Hibernate")
+            {
+                shutdown -h -f
+            }
+        }
+
+        $job = Start-Job -Name "ScheduledShutdown" -ScriptBlock $scriptBlock -ArgumentList $Task, $Time
     }
-    elseif($Task -eq "Restart")
+    else
     {
-        shutdown -r -f -t $time
-    }
-    elseif($Task -eq "Log off")
-    {
-        shutdown -l -f -t $time
-    }
-    elseif($Task -eq "Hibernate")
-    {
-        shutdown -h -f
-    }
-    elseif($Task -eq "Abort")
-    {
+        Stop-Job -Name "ScheduledShutdown"
+        Remove-Job -Name "ScheduledShutdown"
         shutdown -a
     }
 }
