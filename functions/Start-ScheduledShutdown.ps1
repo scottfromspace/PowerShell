30c318677e2bee0f99f44006d987867fb4122169
scottfromspace <scott.serpenz@gmail.com>
2023-10-21 22:12:04 +1300

    Update Start-ScheduledShutdown.ps1

--- functions/Start-ScheduledShutdown.ps1
+++ functions/Start-ScheduledShutdown.ps1
@@ -7,24 +7,28 @@ function Compare-ADUsers {
         [string]$Task,
         [string]$Hours,
         [string]$Minutes,
-        [string]$Seconds
+        [string]$Seconds,
+        [bool]$DisableWarning
     )
+
+    $time = 0
+    $time = $time + ((($Hours * 60) * 60) + ($Minutes * 60) + ($Seconds))
 
     if($Task -eq "Shutdown")
     {
-        shutdown -s
+        shutdown -s -f -t $time
     }
     elseif($Task -eq "Restart")
     {
-        shutdown -r
-    }
-    elseif($Task -eq "Hibernate")
-    {
-        shutdown -h
+        shutdown -r -f -t $time
     }
     elseif($Task -eq "Log off")
     {
-        shutdown -l
+        shutdown -l -f -t $time
+    }
+    elseif($Task -eq "Hibernate")
+    {
+        shutdown -h -f
     }
     elseif($Task -eq "Abort")
     {
