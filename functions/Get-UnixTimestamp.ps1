2143a4d6fb0fee47e6ffc398a2b1d6156914ed7d
scottfromspace <scott.serpenz@gmail.com>
2023-10-22 21:37:10 +1300

    Create Get-UnixTimestamp.ps1

---
+++ functions/Get-UnixTimestamp.ps1
@@ -0,0 +1,23 @@
+#Serpenz Software, https://www.serpenz.co.nz/
+
+function Get-UnixTimestamp {
+    param (
+        [int]$addSeconds = 0,
+        [int]$addMinutes = 0,
+        [int]$addHours = 0,
+        [int]$subtractSeconds = 0,
+        [int]$subtractMinutes = 0,
+        [int]$subtractHours = 0
+    )
+    
+    $date = Get-Date
+    $date = $date.AddSeconds($addSeconds)
+    $date = $date.AddMinutes($addMinutes)
+    $date = $date.AddHours($addHours)
+    $date = $date.AddSeconds(-$subtractSeconds)
+    $date = $date.AddMinutes(-$subtractMinutes)
+    $date = $date.AddHours(-$subtractHours)
+    
+    $timestamp = $date.ToUniversalTime().Subtract((Get-Date "1970-01-01 00:00:00")).TotalSeconds
+    return [math]::Round($timestamp)
+}
