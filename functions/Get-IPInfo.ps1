50ab9db06d34e9e628eda59e7ec08349c6a198bf
scottfromspace <scott.serpenz@gmail.com>
2023-10-26 22:36:23 +1300

    Create Get-IPInfo.ps1

---
+++ functions/Get-IPInfo.ps1
@@ -0,0 +1,14 @@
+﻿#Serpenz Software, https://www.serpenz.co.nz/
+
+function Get-IPInfo {
+    param (
+        [string]$IPAddress
+    )
+
+    if($IPAddress.Length -eq 0)
+    {
+        $ipAddress = (Invoke-WebRequest -Uri "https://ipinfo.io/ip").Content.Trim()
+    }
+
+    Invoke-RestMethod -Uri "https://ipinfo.io/$ipAddress/json"
+}
