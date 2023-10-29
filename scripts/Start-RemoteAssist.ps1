15267821ac6a8909744af4a2579c54b12e504924
scottfromspace <scott.serpenz@gmail.com>
2023-10-29 21:56:06 +1300

    clean-up. removed files.

--- scripts/Start-RemoteAssist.ps1
+++
@@ -1,5 +0,0 @@
-﻿#Serpenz Software, https://www.serpenz.co.nz/
-
-$recipientIP = Read-Host "Enter the IP of to remote assist"
-
-msra.exe /offerra $recipientIP
--- scripts/Update-Help.ps1
+++
@@ -1,3 +0,0 @@
-#Serpenz Software, https://www.serpenz.co.nz/
-
-update-help -force -ea 0
